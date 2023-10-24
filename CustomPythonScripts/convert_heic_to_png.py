d6e002eeb4e9be6c12446aaace888c9c80e24e79
Furkan Gözükara <furkangozukara@gmail.com>
2023-10-24 21:28:12 +0300

    Update convert_heic_to_png.py

--- CustomPythonScripts/convert_heic_to_png.py
+++ CustomPythonScripts/convert_heic_to_png.py
@@ -1,28 +1,37 @@
-#pip install imageio imageio[freeimage]
-#Please donate SECourses https://www.patreon.com/SECourses
+# https://imagemagick.org/script/download.php 
+# Please donate SECourses https://www.patreon.com/SECourses
 
 import os
-import imageio
+import subprocess
 
-def heic_to_png(input_folder, output_folder):
-    # Ensure the output folder exists
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
+def convert_heic_to_png(input_folder_path, output_folder_path):
+    """Converts all HEIC images in the input folder to PNG images in the output folder using ImageMagick.
 
-    # Loop through all files in the input folder
-    for filename in os.listdir(input_folder):
+    Args:
+        input_folder_path: The path to the input folder containing the HEIC images.
+        output_folder_path: The path to the output folder where the converted PNG images will be saved.
+    """
+    if not os.path.exists(output_folder_path):
+        os.makedirs(output_folder_path)
+
+    for filename in os.listdir(input_folder_path):
         if filename.lower().endswith(".heic"):
-            heic_path = os.path.join(input_folder, filename)
-            # Read the HEIC image
-            image = imageio.imread(heic_path)
+            input_file_path = os.path.join(input_folder_path, filename)
+            output_file_path = os.path.join(output_folder_path, os.path.splitext(filename)[0] + ".png")
             
-            # Create the output path
-            output_path = os.path.join(output_folder, os.path.splitext(filename)[0] + '.png')
-            # Save the image as PNG
-            imageio.imwrite(output_path, image)
+            try:
+                # Constructing the ImageMagick convert command
+                command = ["magick", input_file_path, output_file_path]
+                
+                # Executing the command
+                subprocess.run(command, check=True)
+                print(f"Conversion successful: {input_file_path} to {output_file_path}")
+            except subprocess.CalledProcessError as e:
+                print(f"ImageMagick conversion failed: {str(e)}")
+            except Exception as e:
+                print(f"An error occurred: {str(e)}")
 
 if __name__ == "__main__":
-    INPUT_FOLDER = "/path/to/input_folder"
-    OUTPUT_FOLDER = "/path/to/output_folder"
-    
-    heic_to_png(INPUT_FOLDER, OUTPUT_FOLDER)
+    input_folder_path = r"input_folder"
+    output_folder_path = r"output_folder"
+    convert_heic_to_png(input_folder_path, output_folder_path)
